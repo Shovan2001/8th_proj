4ecf217e77a377b12d480be11359133bf6926e2f
Shovan Akhuli <shovanakhuli@gmail.com>
2023-04-18 21:54:33 +0530

    18/04

---
+++ src/Data/PostsData.js
@@ -0,0 +1,63 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.jpg'
+import post4 from '../img/post4.jpeg'
+import post5 from '../img/post5.jpg'
+import post6 from '../img/post6.jpeg'
+import post7 from '../img/post7.jpeg'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Rajesh',
+        desc: "Nice picture ",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Sayan',
+        desc: "❤️❤️❤️",
+        likes: 2300,
+        liked: false
+
+    },
+    {
+        img: post4,
+        name: 'Rajesh',
+        desc: "Nice picture ",
+        likes: 2300,
+        liked: true
+    },
+    
+    {
+        img:postPic3,
+        name: "Suraj",
+        desc: "Everything is perfect",
+        likes: 800,
+        liked: false
+    },
+    {
+        img: post5,
+        name: 'Rajesh',
+        desc: "Nice picture ",
+        likes: 2300,
+        liked: true
+    },
+    
+    {
+        img: post6,
+        name: 'Rajesh',
+        desc: "Nice picture ",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: post7,
+        name: 'Rajesh',
+        desc: "Nice picture ",
+        likes: 2300,
+        liked: true
+    },
+    
+]
